2d29107f47a53ac73f53c0b3a7a578a56746c58e
ch3f <jreyes-s@student.42madrid.com>
2026-02-12 22:41:42 +0100

    add nvim.bkp

---
+++ config/nvim.bkp/lua/plugins/42header.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "42Paris/42header",
+    lazy = false,
+    config = function()
+      vim.g.user42 = "jreyes-s"
+      vim.g.mail42 = "jreyes-s@student.42madrid.com"
+    end,
+  },
+}
